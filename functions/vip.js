99e8a8945ad419b306ec9aa777e7800654cedbdb
lalyos <lalyos@yahoo.com>
2023-04-28 22:31:28 +0200

    list all vip not just first

--- functions/vip.js
+++ functions/vip.js
@@ -1,6 +1,7 @@
 export async function onRequest(context) {
   const ps = context.env.TEST_DB.prepare('SELECT * from vip');
-  const data = await ps.first();
+  // const data = await ps.first();
+  const data = await ps.all();
 
   return Response.json(data);
 }

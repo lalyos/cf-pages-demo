d235f99103c78e0570162bec830789ac8f3e3a78
lalyos <lalyos@yahoo.com>
2024-09-02 12:04:46 +0200

    v4

--- functions/hello.js
+++ functions/hello.js
@@ -1,3 +1,3 @@
 export function onRequest(context) {
-    return new Response("Hello, world! v3")
+    return new Response("Hello, world! v4")
 }
--- functions/vip.js
+++ functions/vip.js
@@ -2,7 +2,10 @@ export async function onRequest(context) {
 
   const ps = context.env.TEST_DB.prepare('SELECT * from vip');
   // const data = await ps.first();
-  console.log("context.env.TEST_DB:", context.env.TEST_DB)
+  console.log("context.env.TEST_DB:", context.env.TEST_DB.name);
+  console.dir(context.env.TEST_DB, { depth: null });
+
+
   const data = await ps.all();
 
   return Response.json(data.results);

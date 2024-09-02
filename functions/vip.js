6e7b4f2a72fde7ba2eb72158217e25492ed49f46
lalyos <lalyos@yahoo.com>
2024-09-02 11:59:23 +0200

    v3

--- functions/vip.js
+++ functions/vip.js
@@ -1,6 +1,8 @@
 export async function onRequest(context) {
+
   const ps = context.env.TEST_DB.prepare('SELECT * from vip');
   // const data = await ps.first();
+  console.log("context.env.TEST_DB:", context.env.TEST_DB)
   const data = await ps.all();
 
   return Response.json(data.results);
